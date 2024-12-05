8392cc642f41bec4d25970a85dbfbf346ba9d377
Amiya Ranjan <amiya3769@gmail.com>
2024-12-05 09:45:56 +0530

    Add files via upload

---
+++ day37.js
@@ -0,0 +1,12 @@
+// Assign ment operators in java script
+// 1-Arithmetic operator
+// let a=4;
+// let b=45;
+// console.log("a+b",a+b);
+// console.log("a-b",a-b);
+// console.log("a*b",a*b)
+//  Unary opetator
+let a=5;
+let b=2;
+a++;
+console.log(a);
